04042f5190bbc81575efb48b2a4af7483b901e6d
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-05-17 00:58:00 +0200

    feat: Add negative test cases for `MarkOffsetPushable`

--- pkg/chunks/pusher_test.go
+++ pkg/chunks/pusher_test.go
@@ -10,25 +10,36 @@ import (
 
 func TestPusher(t *testing.T) {
 	tests := []struct {
-		name       string
-		chunkSize  int64
-		chunks     int64
-		pushPeriod time.Duration
-		data       [][]byte
+		name                string
+		chunkSize           int64
+		chunks              int64
+		pushPeriod          time.Duration
+		data                [][]byte
+		markOffsetsPushable bool
 	}{
 		{
-			name:       "Push 1 chunk",
-			chunkSize:  4,
-			chunks:     2,
-			pushPeriod: time.Second,
-			data:       [][]byte{[]byte("test")},
+			name:                "Push 1 chunk",
+			chunkSize:           4,
+			chunks:              2,
+			pushPeriod:          time.Second,
+			data:                [][]byte{[]byte("test")},
+			markOffsetsPushable: true,
 		},
 		{
-			name:       "Push 2 chunks",
-			chunkSize:  4,
-			chunks:     2,
-			pushPeriod: time.Second,
-			data:       [][]byte{[]byte("test"), []byte("test")},
+			name:                "Push 2 chunks",
+			chunkSize:           4,
+			chunks:              2,
+			pushPeriod:          time.Second,
+			data:                [][]byte{[]byte("test"), []byte("test")},
+			markOffsetsPushable: true,
+		},
+		{
+			name:                "Do not push chunks without MarkOffsetPushable",
+			chunkSize:           4,
+			chunks:              2,
+			pushPeriod:          time.Second,
+			data:                [][]byte{[]byte("test"), []byte("test")},
+			markOffsetsPushable: false,
 		},
 	}
 
@@ -70,9 +81,11 @@ func TestPusher(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			for i := int64(0); i < tc.chunks; i++ {
-				if err := pusher.MarkOffsetPushable(i * tc.chunkSize); err != nil {
-					t.Fatal(err)
+			if tc.markOffsetsPushable {
+				for i := int64(0); i < tc.chunks; i++ {
+					if err := pusher.MarkOffsetPushable(i * tc.chunkSize); err != nil {
+						t.Fatal(err)
+					}
 				}
 			}
 
@@ -92,14 +105,28 @@ func TestPusher(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			for i, chunk := range tc.data {
-				remoteData := make([]byte, len(chunk))
-				if _, err := remote.ReadAt(remoteData, int64(i)*tc.chunkSize); err != nil {
-					t.Fatal(err)
-				}
+			if !tc.markOffsetsPushable {
+				expectedRemoteData := make([]byte, tc.chunkSize)
+				for i, _ := range tc.data {
+					remoteData := make([]byte, len(expectedRemoteData))
+					if _, err := remote.ReadAt(remoteData, int64(i)*tc.chunkSize); err != nil {
+						t.Fatal(err)
+					}
 
-				if !bytes.Equal(remoteData, chunk) {
-					t.Errorf("Data pushed did not match expected. got %v, want %v", remoteData, tc.data)
+					if !bytes.Equal(remoteData, expectedRemoteData) {
+						t.Errorf("Chunks were pushed without calling MarkOffsetPushable. got %v, want %v", remoteData, expectedRemoteData)
+					}
+				}
+			} else {
+				for i, chunk := range tc.data {
+					remoteData := make([]byte, len(chunk))
+					if _, err := remote.ReadAt(remoteData, int64(i)*tc.chunkSize); err != nil {
+						t.Fatal(err)
+					}
+
+					if !bytes.Equal(remoteData, chunk) {
+						t.Errorf("Data pushed did not match expected. got %v, want %v", remoteData, tc.data)
+					}
 				}
 			}
 		})
