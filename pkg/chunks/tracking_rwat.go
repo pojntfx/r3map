acc193e8104f155e65e45ca0c256dad747809ce3
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-06-17 23:57:01 +0200

    feat: Start implementation of write-tracking `rwat`

---
+++ pkg/chunks/tracking_rwat.go
@@ -0,0 +1,56 @@
+package chunks
+
+import "sync"
+
+type TrackingReadWriterAt struct {
+	backend ReadWriterAt
+
+	tracking     bool
+	trackingLock sync.Mutex
+
+	dirtyOffsets map[int64]struct{}
+}
+
+func NewTrackingReadWriterAt(backend ReadWriterAt) *TrackingReadWriterAt {
+	return &TrackingReadWriterAt{
+		backend:      backend,
+		dirtyOffsets: map[int64]struct{}{},
+	}
+}
+
+func (c *TrackingReadWriterAt) ReadAt(p []byte, off int64) (n int, err error) {
+	return c.backend.ReadAt(p, off)
+}
+
+func (c *TrackingReadWriterAt) WriteAt(p []byte, off int64) (n int, err error) {
+	c.trackingLock.Lock()
+	defer c.trackingLock.Unlock()
+
+	if c.tracking {
+		c.dirtyOffsets[off] = struct{}{}
+	}
+
+	return c.backend.WriteAt(p, off)
+}
+
+func (c *TrackingReadWriterAt) Flush() []int64 {
+	c.trackingLock.Lock()
+	defer c.trackingLock.Unlock()
+
+	rv := make([]int64, 0, len(c.dirtyOffsets))
+	for off := range c.dirtyOffsets {
+		rv = append(rv, off)
+	}
+
+	c.dirtyOffsets = map[int64]struct{}{}
+	c.tracking = false
+
+	return rv
+}
+
+func (c *TrackingReadWriterAt) Track() {
+	c.trackingLock.Lock()
+	defer c.trackingLock.Unlock()
+
+	c.tracking = true
+}
---
+++ pkg/chunks/tracking_rwat_test.go
@@ -0,0 +1,103 @@
+package chunks
+
+import (
+	"bytes"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestTrackingReadWriterAt(t *testing.T) {
+	tests := []struct {
+		name            string
+		inputs          [][]byte
+		offsets         []int64
+		rwBufferSize    int
+		expectedData    [][]byte
+		expectedOffsets []int64
+		readErr         error
+		writeErr        error
+	}{
+		{
+			name:            "Write and track one chunk",
+			inputs:          [][]byte{[]byte("1234")},
+			offsets:         []int64{0},
+			rwBufferSize:    4,
+			expectedData:    [][]byte{[]byte("1234")},
+			expectedOffsets: []int64{0},
+			readErr:         nil,
+			writeErr:        nil,
+		},
+		{
+			name:            "Write and track two chunks",
+			inputs:          [][]byte{[]byte("1234"), []byte("5678")},
+			offsets:         []int64{0, 4},
+			rwBufferSize:    4,
+			expectedData:    [][]byte{[]byte("1234"), []byte("5678")},
+			expectedOffsets: []int64{0, 4},
+			readErr:         nil,
+			writeErr:        nil,
+		},
+		{
+			name:            "Write and track three chunks",
+			inputs:          [][]byte{[]byte("1234"), []byte("5678"), []byte("9012")},
+			offsets:         []int64{0, 4, 8},
+			rwBufferSize:    4,
+			expectedData:    [][]byte{[]byte("1234"), []byte("5678"), []byte("9012")},
+			expectedOffsets: []int64{0, 4, 8},
+			readErr:         nil,
+			writeErr:        nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			f, err := os.CreateTemp("", "")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.RemoveAll(f.Name())
+
+			trw := NewTrackingReadWriterAt(f)
+
+			if len(tc.inputs) > 0 {
+				trw.Track()
+
+				for i, input := range tc.inputs {
+					wn, werr := trw.WriteAt(input, tc.offsets[i])
+					if werr != tc.writeErr {
+						t.Errorf("WriteAt error: expected %v, got %v", tc.writeErr, werr)
+					}
+
+					if wn != len(input) {
+						t.Errorf("WriteAt bytes written: expected %v, got %v", len(input), wn)
+					}
+				}
+
+				dirtyOffsets := trw.Flush()
+				if !reflect.DeepEqual(tc.expectedOffsets, dirtyOffsets) {
+					t.Errorf("Flush offsets: expected %v, got %v", tc.expectedOffsets, dirtyOffsets)
+				}
+			}
+
+			if len(tc.expectedData) > 0 || tc.readErr != nil {
+				rbuf := make([]byte, tc.rwBufferSize)
+				for i, expected := range tc.expectedData {
+					rn, rerr := trw.ReadAt(rbuf, tc.offsets[i])
+					if rerr != tc.readErr {
+						t.Errorf("ReadAt error: expected %v, got %v", tc.readErr, rerr)
+					}
+
+					if rn != len(expected) {
+						t.Errorf("ReadAt bytes read: expected %v, got %v", len(expected), rn)
+					}
+
+					rbuf = rbuf[:rn]
+					if !bytes.Equal(rbuf, expected) {
+						t.Errorf("ReadAt data: expected %v, got %v", expected, rbuf)
+					}
+				}
+			}
+		})
+	}
+}
