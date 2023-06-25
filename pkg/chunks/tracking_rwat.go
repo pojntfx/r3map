54f7ce7ae7cb24ed29c4adbc63d147f08bd2a572
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-06-25 03:03:23 +0200

    refactor: Split tracking rwat into separate index and store

--- pkg/chunks/tracking_rwat.go
+++ pkg/chunks/tracking_rwat.go
@@ -1,20 +1,23 @@
 package chunks
 
-import "sync"
+import (
+	"sync"
+)
 
 type TrackingReadWriterAt struct {
 	backend ReadWriterAt
 
-	tracking     bool
-	trackingLock sync.Mutex
-
-	dirtyOffsets map[int64]struct{}
+	tracking          bool
+	trackingLock      sync.Mutex
+	dirtyOffsetsIndex map[int64]struct{}
+	dirtyOffsetsStore []int64 // Separate store from the index so that we don't need to convert it the map to an []int64 later
 }
 
 func NewTrackingReadWriterAt(backend ReadWriterAt) *TrackingReadWriterAt {
 	return &TrackingReadWriterAt{
-		backend:      backend,
-		dirtyOffsets: map[int64]struct{}{},
+		backend:           backend,
+		dirtyOffsetsIndex: map[int64]struct{}{},
+		dirtyOffsetsStore: []int64{},
 	}
 }
 
@@ -27,7 +30,10 @@ func (c *TrackingReadWriterAt) WriteAt(p []byte, off int64) (n int, err error) {
 	defer c.trackingLock.Unlock()
 
 	if c.tracking {
-		c.dirtyOffsets[off] = struct{}{}
+		if _, found := c.dirtyOffsetsIndex[off]; !found {
+			c.dirtyOffsetsIndex[off] = struct{}{}
+			c.dirtyOffsetsStore = append(c.dirtyOffsetsStore, off)
+		}
 	}
 
 	return c.backend.WriteAt(p, off)
@@ -37,12 +43,10 @@ func (c *TrackingReadWriterAt) Sync() []int64 {
 	c.trackingLock.Lock()
 	defer c.trackingLock.Unlock()
 
-	rv := make([]int64, 0, len(c.dirtyOffsets))
-	for off := range c.dirtyOffsets {
-		rv = append(rv, off)
-	}
+	rv := c.dirtyOffsetsStore
 
-	c.dirtyOffsets = map[int64]struct{}{}
+	c.dirtyOffsetsIndex = map[int64]struct{}{}
+	c.dirtyOffsetsStore = []int64{}
 	c.tracking = false
 
 	return rv
