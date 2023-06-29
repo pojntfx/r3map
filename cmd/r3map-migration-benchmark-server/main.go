855746a31d2bb2e7dadba7a22b8d449a96b5ffd0
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-06-29 14:18:07 +0200

    feat: Add support for backend selection in migration benchmark server

--- cmd/r3map-migration-benchmark-server/main.go
+++ cmd/r3map-migration-benchmark-server/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"crypto/rand"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -11,34 +12,100 @@ import (
 	"math"
 	"net"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/pojntfx/dudirekta/pkg/rpc"
 	"github.com/pojntfx/go-nbd/pkg/backend"
 	v1frpc "github.com/pojntfx/r3map/pkg/api/frpc/migration/v1"
 	v1proto "github.com/pojntfx/r3map/pkg/api/proto/migration/v1"
+	lbackend "github.com/pojntfx/r3map/pkg/backend"
+	"github.com/pojntfx/r3map/pkg/chunks"
 	"github.com/pojntfx/r3map/pkg/migration"
 	"github.com/pojntfx/r3map/pkg/services"
 	"github.com/pojntfx/r3map/pkg/utils"
 	"google.golang.org/grpc"
 )
 
+const (
+	backendTypeFile      = "file"
+	backendTypeMemory    = "memory"
+	backendTypeDirectory = "directory"
+)
+
+var (
+	knownBackendTypes = []string{backendTypeFile, backendTypeMemory, backendTypeDirectory}
+
+	errUnknownBackend = errors.New("unknown backend")
+)
+
 func main() {
 	laddr := flag.String("laddr", ":1337", "Listen address")
-	size := flag.Int64("size", 4096*8192, "Size of the memory region to expose")
-	slice := flag.Bool("slice", false, "Whether to use the slice frontend instead of the file frontend")
-	chunkSize := flag.Int64("chunk-size", 4096, "Chunk size to use")
 	enableGrpc := flag.Bool("grpc", false, "Whether to use gRPC instead of Dudirekta")
 	enableFrpc := flag.Bool("frpc", false, "Whether to use fRPC instead of Dudirekta")
-	invalidate := flag.Int("invalidate", 0, "Percentage of chunks (0-100) to invalidate in between Track() and Finalize()")
 	verbose := flag.Bool("verbose", false, "Whether to enable verbose logging")
+
+	size := flag.Int64("size", 4096*8192, "Size of the memory region or file to allocate")
+	chunkSize := flag.Int64("chunk-size", 4096, "Chunk size to use")
+	bck := flag.String(
+		"backend",
+		backendTypeFile,
+		fmt.Sprintf(
+			"Backend to use (one of %v)",
+			knownBackendTypes,
+		),
+	)
+	location := flag.String("location", filepath.Join(os.TempDir(), "local"), "Backend's directory (for directory backend)")
+	chunking := flag.Bool("chunking", true, "Whether the backend requires to be interfaced with in fixed chunks in tests")
+
+	slice := flag.Bool("slice", false, "Whether to use the slice frontend instead of the file frontend")
+	invalidate := flag.Int("invalidate", 0, "Percentage of chunks (0-100) to invalidate in between Track() and Finalize()")
 
 	flag.Parse()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	b := backend.NewMemoryBackend(make([]byte, *size))
+	var b backend.Backend
+	switch *bck {
+	case backendTypeMemory:
+		b = backend.NewMemoryBackend(make([]byte, *size))
+
+	case backendTypeFile:
+		file, err := os.CreateTemp("", "")
+		if err != nil {
+			panic(err)
+		}
+		defer os.RemoveAll(file.Name())
+
+		if err := file.Truncate(*size); err != nil {
+			panic(err)
+		}
+
+		b = backend.NewFileBackend(file)
+
+	case backendTypeDirectory:
+		if err := os.MkdirAll(*location, os.ModePerm); err != nil {
+			panic(err)
+		}
+
+		b = lbackend.NewDirectoryBackend(*location, *size, *chunkSize, 512, false)
+	default:
+		panic(errUnknownBackend)
+	}
+
+	if *chunking {
+		b = lbackend.NewReaderAtBackend(
+			chunks.NewArbitraryReadWriterAt(
+				chunks.NewChunkedReadWriterAt(
+					b, *chunkSize, *size / *chunkSize),
+				*chunkSize,
+			),
+			(b).Size,
+			(b).Sync,
+			false,
+		)
+	}
 
 	var (
 		svc               *services.Seeder
