ceaac69a4d870e031c435695e2d3c1dd1d25cb74
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-08-10 22:04:09 +0200

    feat: Add example mount server

---
+++ cmd/r3map-example-mount-server/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"os"
+
+	"github.com/pojntfx/go-nbd/pkg/backend"
+	v1 "github.com/pojntfx/r3map/pkg/api/proto/mount/v1"
+	"github.com/pojntfx/r3map/pkg/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	size := flag.Int64("size", 4096*8192*10, "Size of the resource")
+
+	maxChunkSize := flag.Int64("max-chunk-size", services.MaxChunkSize, "Maximum chunk size to support")
+
+	laddr := flag.String("laddr", "localhost:1337", "Listen address")
+
+	verbose := flag.Bool("verbose", false, "Whether to enable verbose logging")
+
+	flag.Parse()
+
+	f, err := os.CreateTemp("", "")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(f.Name())
+
+	if err := f.Truncate(*size); err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+
+	v1.RegisterBackendServer(
+		srv,
+		services.NewBackendServiceGrpc(
+			services.NewBackend(
+				backend.NewFileBackend(f),
+				*verbose,
+				*maxChunkSize,
+			),
+		),
+	)
+
+	lis, err := net.Listen("tcp", *laddr)
+	if err != nil {
+		panic(err)
+	}
+	defer lis.Close()
+
+	log.Println("Listening on", *laddr)
+
+	if err := srv.Serve(lis); err != nil {
+		panic(err)
+	}
+}
